223a99307688bc2527c2d2517e9babcd49b87d9b
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-09-24 23:34:47 -0400

    works as mass action using * to mark, x e

--- mu4e-save-to-eml.el
+++ mu4e-save-to-eml.el
@@ -1,4 +1,4 @@
-
+;; -*- lexical-binding: t; -*-
 
 (defun djcb-mu4e-copy-message-at-point (&optional dir)
   "Copy message at point to somewhere else as <date>_<subject>.eml."
@@ -14,7 +14,9 @@
      (format "%s/%s" (or dir (read-directory-name "Copy message to: ")) target) 1)))
 
 
-(defun xxx-test ()
+(defun mu4e-save-to-eml (&optional msg dir)
+  (interactive)
+  (unless msg (setq msg (mu4e-message-at-point)))
   (cl-flet* ((get-display-name
 	      (cell)
 	      (if (car cell)
@@ -31,24 +33,33 @@
 		    (cdr cell)
 		    "")))
 	     (make-file-name (msg)
-			     (let* ((msg (mu4e-message-at-point))
-				    (to-field (plist-get msg :to))
-				    (first-to (car to-field))
-				    (to-etal-p (cdr to-field))
-				    ;; if there is a name in the to field
-				    (from (car (plist-get msg :from))))
-			       (concat 
-				(format-time-string "%F" (mu4e-message-field msg :date))
-				" - "
-				(get-display-name from)
-				" to "
-				(get-display-name first-to)
-				(if to-etal-p " et al" "")
-				" - "
-				(or (mu4e-message-field msg :subject) "No subject")))))
-    (cl-loop for x from 1 to (1- (count-lines (point-min) (point-max)))
-	     collect (make-file-name (mu4e-message-at-point))
-	     do (next-line 1)
-	     )))
+			     (s-replace "/" ""
+					(let* ((msg (mu4e-message-at-point))
+					       (to-field (plist-get msg :to))
+					       (first-to (car to-field))
+					       (to-etal-p (cdr to-field))
+					       ;; if there is a name in the to field
+					       (from (car (plist-get msg :from))))
+					  (concat 
+					   (format-time-string "%F" (mu4e-message-field msg :date))
+					   " - "
+					   (get-display-name from)
+					   " to "
+					   (get-display-name first-to)
+					   (if to-etal-p " et al" "")
+					   " - "
+					   (or (mu4e-message-field msg :subject) "No subject")
+					   ".eml")))))
+    (copy-file
+     (mu4e-message-field msg :path)
+     (concat 
+      (or dir
+	  (read-directory-name "Copy message to: "))
+      "/"
+      (make-file-name msg))
+     1)))
+
+
+
 
 
